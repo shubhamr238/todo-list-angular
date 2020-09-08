d15bb913d70637494b424987e285506c79ffea16
shubhamr238 <shubhamr238@gmail.com>
2020-09-08 16:15:34 +0530

    add Toggle and Delete events

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -20,4 +20,10 @@ export class TodoItemComponent implements OnInit {
     };
     return classes;
   }
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+  onDelete(todo) {
+    console.log('delete');
+  }
 }
